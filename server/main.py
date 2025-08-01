bfb9ecb491325534e8c9c8f98aff44e27906a2eb
Sahil-796 <sp0709006@gmail.com>
2025-08-01 11:07:13 +0530

    migrated from vertex to gemini. Redfined requirements.txt for vlean and efficient setup

--- server/main.py
+++ server/main.py
@@ -1,14 +1,33 @@
-from fastapi import FastAPI
 import os
+from fastapi import FastAPI, UploadFile, File
 from dotenv import load_dotenv
 
-load_dotenv()
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+from pinecone import Pinecone
+from langchain_google_vertexai import VertexAIEmbeddings
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_pinecone import PineconeVectorStore
 
+load_dotenv()
 app = FastAPI()
 
+pc = Pinecone(
+    api_key=os.getenv("PINECONE_API_KEY"),
+)
+index_name = os.getenv("PINECONE_INDEX")
+index = pc.Index(index_name)  
+
+
+embeddings = VertexAIEmbeddings(
+model_name='text-embedding-004'
+)
 
 @app.get('/')
 def root():
     return {"message": "Welcome to the FastAPI server!"}
 
+@app.post('/upload')
+async def upload_file(file: UploadFile = File(...)):
+    print(file)
+    return {"filename":file.filename}
