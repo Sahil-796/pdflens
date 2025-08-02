09858a09bb4fc0e4a43c5b1de115ada83f12beaf
Sahil-796 <sp0709006@gmail.com>
2025-08-02 22:30:55 +0530

    Successfully created embeddinsg and stored it in pineconeDB. /upload is now working.

--- server/main.py
+++ server/main.py
@@ -1,8 +1,10 @@
 import os
-from fastapi import FastAPI, UploadFile, File
+import tempfile
+from fastapi import FastAPI, UploadFile, File, Form
 from dotenv import load_dotenv
 
-from pinecone import Pinecone
+from pinecone import Pinecone, ServerlessSpec
+from langchain_pinecone import PineconeVectorStore
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_community.document_loaders import PyPDFLoader
@@ -12,11 +14,19 @@ load_dotenv()
 app = FastAPI()
 
 pc = Pinecone(
-    api_key=os.getenv("PINECONE_API_KEY"),
+    api_key=os.environ.get("PINECONE_API_KEY"),
 )
-index_name = os.getenv("PINECONE_INDEX")
-index = pc.Index(index_name)  
 
+INDEX_NAME = os.getenv("PINECONE_INDEX")
+
+if not pc.has_index(INDEX_NAME):
+    pc.create_index(
+        name=INDEX_NAME,
+        dimension=3072,
+        metric="cosine",
+        spec=ServerlessSpec(cloud="aws", region="us-east-1")
+    )
+index = pc.Index(INDEX_NAME)  
 
 embeddings = GoogleGenerativeAIEmbeddings(model='models/gemini-embedding-001')
 
@@ -27,7 +37,46 @@ embeddings = GoogleGenerativeAIEmbeddings(model='models/gemini-embedding-001')
 def root():
     return {"message": "Welcome to the FastAPI server!"}
 
+
 @app.post('/upload')
-async def upload_file(file: UploadFile = File(...)):
-    print(file)
-    return {"filename":file.filename}
+async def upload_context(file: UploadFile = File(...), userId: str = Form(...)):
+
+    extension = file.filename.split('.')[-1].lower()
+    
+
+    with tempfile.NamedTemporaryFile(delete=False, suffix=f".{extension}") as tmp:
+        contents = await file.read()
+        tmp.write(contents)
+        tmp_path = tmp.name
+
+    if extension == 'pdf':
+        loader = PyPDFLoader(tmp_path)
+    elif (extension == 'doc') or (extension == 'docx'):
+        return {"message":"doc or docx"}
+    else:
+        return {"error":"Unsupported file type"}
+    
+    
+    documents = loader.load()
+
+    splitter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap=50)
+    chunks = splitter.split_documents(documents)
+
+    for i, chunk in enumerate(chunks):
+        chunk.metadata.update({
+            "userId": userId,
+            "filename": file.filename,
+            "chunkId": i
+        })
+
+    vectorStore = PineconeVectorStore.from_documents(
+        documents=chunks,
+        embedding=embeddings,
+        index_name=INDEX_NAME
+    )
+    
+
+
+    return {
+        "message": "File uplaoded and indexed",
+    }
