8f8654a480df065a04e69b37ad92078bf60a87ad
RudraPatel5435 <rudranpatel0980@gmail.com>
2025-09-10 11:12:58 +0530

    major: created new next-app

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from '@clerk/nextjs'
 import Navbar from "./components/Navbar";
+import { ThemeProvider } from "next-themes";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,10 +31,12 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          <Navbar />
-          <div className="pt-24 px-5">
-          {children}
-          </div>
+          <ThemeProvider attribute='class' defaultTheme="dark">
+            <Navbar />
+            <div className="pt-24 px-5">
+              {children}
+            </div>
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
