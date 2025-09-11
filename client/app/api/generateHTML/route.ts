16fcb403305089a974395c51215e4d591bc2198d
Sahil-796 <sp0709006@gmail.com>
2025-09-11 11:54:11 +0530

    add: generateHtml api

--- client/app/api/generateHTML/route.ts
+++ client/app/api/generateHTML/route.ts
@@ -1,15 +1,20 @@
-export async function generateHTML(input: string) {
-    const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:8000/ai/generate'
-    const res = await fetch(BASE_URL, {
+import { NextResponse } from "next/server"
+
+export async function POST(req: Request){
+    const { user_id, user_prompt } = await req.json()
+    const PYTHON_URL = process.env.NEXT_PYTHON_URL || 'http://localhost:8000/ai/generate'
+
+    const res = await fetch(PYTHON_URL, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
+            "x-api-key": "any random key here"
         },
         body: JSON.stringify({
-            user_id: "sahil7",
-            user_prompt: input
+            user_id: user_id,
+            user_prompt: user_prompt
         })
     })
     let data = await res.json()
-    return data
+    return NextResponse.json({data})
 }
