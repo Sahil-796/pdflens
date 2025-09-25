9909f35fa8591bfa407e69686160a0a4b4ffa013
Sahil-796 <sp0709006@gmail.com>
2025-09-26 00:15:48 +0530

    schema change for accomodating creit system add: credits.ts for credit functionality

---
+++ client/db/credits.ts
@@ -0,0 +1,55 @@
+import { db } from "./client"
+import { user } from "./schema"
+import { eq } from 'drizzle-orm';
+import { formatInTimeZone } from 'date-fns-tz'
+
+const DAILY_ALLOWANCE = {
+    free: 20,
+    premium: 50, 
+};
+
+export const useCredits = async (userId: string, cost: number) => {
+    try {
+        const result = await db.transaction(async (tx) => {
+            const [currentUser] = await tx.select().from(user).where(eq(user.id, userId));
+
+            if (!currentUser) {
+                throw new Error(`User not found with id: ${userId}`);
+            }
+
+            const todayUTC = formatInTimeZone(new Date(), 'UTC', 'yyyy-MM-dd');
+            let currentCredits = currentUser.creditsLeft;
+
+            // Check if a reset is needed
+            if (currentUser.creditsResetAt < todayUTC) {
+                currentCredits = DAILY_ALLOWANCE[currentUser.plan] - cost;
+                const [updatedUser] = await tx.update(user)
+                    .set({ creditsLeft: currentCredits, creditsResetAt: todayUTC })
+                    .where(eq(user.id, userId))
+                    .returning({ creditsLeft: user.creditsLeft })
+
+                return updatedUser.creditsLeft
+            }
+
+            if (currentCredits < cost) {
+                throw new Error("Insufficient credits"); 
+            }
+
+            const newCreditTotal = currentCredits - cost;
+            const [updatedUser] = await tx.update(user)
+                .set({ creditsLeft: newCreditTotal })
+                .where(eq(user.id, userId))
+                .returning({ creditsLeft: user.creditsLeft });
+                
+
+            return updatedUser.creditsLeft;
+        });
+        return result;
+
+    } catch (err: any) {
+        if (err.message === "Insufficient credits") {
+            return null; 
+        }
+        throw new Error(`Failed to use credits: ${err.message}`);
+    }
+}
