e0ca168da1fa14d702b51ab442c364a8286970f8
RudraPatel5435 <rudranpatel0980@gmail.com>
2025-10-07 12:56:58 +0530

    edit: supremacy

--- client/drizzle/0003_noisy_hellion.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "context" DROP CONSTRAINT "context_pdf_id_pdf_id_fk";
---> statement-breakpoint
-ALTER TABLE "context" ADD CONSTRAINT "context_pdf_id_pdf_id_fk" FOREIGN KEY ("pdf_id") REFERENCES "public"."pdf"("id") ON DELETE cascade ON UPDATE no action;
