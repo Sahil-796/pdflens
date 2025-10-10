cbf55631176712ba2bc32150bfd5836d67dee20d
RudraPatel5435 <rudranpatel0980@gmail.com>
2025-10-10 19:03:25 +0530

    fix: logged out use doeesn't see verify email banner

--- client/components/bars/title-nav.tsx
+++ client/components/bars/title-nav.tsx
@@ -2,43 +2,12 @@ import React from 'react'
 import { SidebarTrigger } from '../ui/sidebar'
 import { Separator } from '../ui/separator'
 import ThemeToggle from '../theme/ThemeToggle'
-import {
-    AlertDialog,
-    AlertDialogCancel,
-    AlertDialogContent,
-    AlertDialogDescription,
-    AlertDialogFooter,
-    AlertDialogHeader,
-    AlertDialogTitle,
-    AlertDialogTrigger,
-} from "@/components/ui/alert-dialog"
-import { Button } from "@/components/ui/button"
-import StickyBanner from '@/components/ui/sticky-banner'
+import VerifyEmailBanner from './verify-email-banner'
 
 const TitleNav = ({ text }: { text: string }) => {
     return (
         <div className='flex flex-col'>
-            <StickyBanner hideOnScroll={true}>
-                <div className="flex items-center justify-center gap-2">
-                    <span>Please check your email to verify your accout.</span>
-                    <AlertDialog>
-                        <AlertDialogTrigger asChild>
-                            <Button variant="outline" size="sm">Resend Email</Button>
-                        </AlertDialogTrigger>
-                        <AlertDialogContent>
-                            <AlertDialogHeader>
-                                <AlertDialogTitle>Verification Email</AlertDialogTitle>
-                                <AlertDialogDescription>
-                                    We have sent you a new verification email, please check both your inbox and spam folder.
-                                </AlertDialogDescription>
-                            </AlertDialogHeader>
-                            <AlertDialogFooter>
-                                <AlertDialogCancel>Close</AlertDialogCancel>
-                            </AlertDialogFooter>
-                        </AlertDialogContent>
-                    </AlertDialog>
-                </div>
-            </StickyBanner>
+            <VerifyEmailBanner />
             <header className="flex border-b-1 border-b-secondary h-16 shrink-0 justify-between items-center gap-2 px-6 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
                 <div className="flex items-center gap-2 ">
                     <SidebarTrigger className="-ml-1" />
