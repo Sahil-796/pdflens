a94045d8fddb698be77092708702ea2c21583a19
RudraPatel5435 <rudranpatel0980@gmail.com>
2025-10-26 11:35:35 +0530

    fix: remove generate suspense

--- client/app/(app)/generate/page.tsx
+++ client/app/(app)/generate/page.tsx
@@ -1,20 +1,13 @@
 import Generate from '@/components/generatePage/Generate'
 import TitleNav from '@/components/bars/title-nav'
-import React, { Suspense } from 'react'
-import LumaSpin from '@/components/21st/LumaSpin'
+import React from 'react'
 
 const page = () => {
   return (
     <div className='h-screen flex flex-col'>
       <TitleNav text="Generate PDF" />
       <div className='flex-1 overflow-auto'>
-        <Suspense fallback={
-          <div className="h-full w-full flex items-center justify-center">
-            <LumaSpin />
-          </div>
-        }>
-          <Generate />
-        </Suspense>
+        <Generate />
       </div>
     </div>
   )
