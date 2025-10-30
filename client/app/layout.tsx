e350fb68a7fc995cb789116322dd76fc5eb0c8d7
RudraPatel5435 <rudranpatel0980@gmail.com>
2025-10-30 21:45:52 +0530

    fix: base url -> public

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -4,44 +4,46 @@ import "./globals.css";
 import { ThemeProvider } from "next-themes";
 import { Toaster } from "sonner";
 import { CommandPaletteProvider } from "@/components/providers/CommandPaletteProvider";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-    title: "Zendra",
-    description: "Zendra - Create, edit, and manage your PDFs with ease.",
+  title: "Zendra",
+  description: "Zendra - Create, edit, and manage your PDFs with ease.",
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    return (
-        <html lang="en" suppressHydrationWarning>
-            <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-            >
-                <ThemeProvider
-                    attribute="class"
-                    defaultTheme="dark"
-                    enableSystem
-                    disableTransitionOnChange
-                >
-                    <CommandPaletteProvider>
-                        {children}
-                        <Toaster position="top-center" richColors />
-                    </CommandPaletteProvider>
-                </ThemeProvider>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <Analytics />
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <CommandPaletteProvider>
+            {children}
+            <Toaster position="top-center" richColors />
+          </CommandPaletteProvider>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
 }
