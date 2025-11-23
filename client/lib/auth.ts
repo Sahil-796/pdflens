607cd6f5d2d341292a4a9faa4b02398994a2dc75
RudraPatel5435 <rudranpatel0980@gmail.com>
2025-11-23 13:30:25 +0530

    feat: verify email

--- client/lib/auth.ts
+++ client/lib/auth.ts
@@ -1,4 +1,4 @@
-import { createClient } from "@supabase/supabase-js"
+import { createClient } from "@supabase/supabase-js";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/db/client";
@@ -8,57 +8,58 @@ import { Resend } from "resend";
 import EmailVerification from "@/components/emails/verify-email";
 
 export const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-)
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+);
 
 const resend = new Resend(process.env.RESEND_API_KEY!);
 
 export const auth = betterAuth({
-    database: drizzleAdapter(db, {
-        provider: "pg",
-        schema,
-    }),
-    user: {
-        deleteUser: {
-            enabled: true,
-        },
-        changeEmail: {
-            enabled: true,
-            sendChangeEmailVerification: async ({ newEmail, url }) => {
-                await resend.emails.send({
-                    from: 'delivered@resend.dev',
-                    to: newEmail,
-                    subject: 'Approve email change',
-                    text: `Click the link to approve the change: ${url}`
-                })
-            }
-        }
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    schema,
+  }),
+  user: {
+    deleteUser: {
+      enabled: true,
     },
-    emailAndPassword: {
-        enabled: true,
-        // requireEmailVerification: true,
+    changeEmail: {
+      enabled: true,
+      sendChangeEmailVerification: async ({ newEmail, url }) => {
+        await resend.emails.send({
+          from: "zendrapdf@sahilpatel.me",
+          to: newEmail,
+          subject: "Approve email change",
+          text: `Click the link to approve the change: ${url}`,
+        });
+      },
     },
-    socialProviders: {
-        google: {
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-        }
+  },
+  emailAndPassword: {
+    enabled: true,
+    requireEmailVerification: true,
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
-    emailVerification: {
-        sendVerificationEmail: async ({ user, url }) => {
-            await resend.emails.send({
-                from: 'delivered@resend.dev',
-                to: user.email!,
-                subject: 'Verify your email',
-                react: EmailVerification({ userEmail: user.email!, emailUrl: url }),
-            });
-        },
-        // sendOnSignUp: true,
+  },
+  emailVerification: {
+    sendVerificationEmail: async ({ user, url }) => {
+      await resend.emails.send({
+        from: "zendrapdf@sahilpatel.me",
+        to: user.email!,
+        subject: "Verify your email",
+        react: EmailVerification({ userEmail: user.email!, emailUrl: url }),
+      });
     },
-    session: {
-        expiresIn: 60 * 60 * 24 * 7,
-        updateAge: 60 * 60 * 24
-    },
-    plugins: [nextCookies()],
+    sendOnSignUp: true,
+  },
+  session: {
+    expiresIn: 60 * 60 * 24 * 7,
+    updateAge: 60 * 60 * 24,
+  },
+  plugins: [nextCookies()],
 });
+
